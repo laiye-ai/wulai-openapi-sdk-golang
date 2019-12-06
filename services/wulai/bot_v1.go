d5a39aaff75e6ab7086f60afe047332082d9c547
shzy2012 <shzy2012@gmail.com>
2019-12-06 14:40:36 +0800

    获取bot回复,返回[]byte

--- services/wulai/bot_v1.go
+++ services/wulai/bot_v1.go
@@ -7,10 +7,11 @@ import (
 //msgBotResponseV1 获取机器人回复(V1)
 func (x *Client) msgBotResponseV1(userID, msgType string, msgBody []byte) ([]byte, error) {
 	url := fmt.Sprintf("%s/%s/msg/bot-response", x.Endpoint, x.Version)
-	input := fmt.Sprintf(`{
+	input := fmt.Sprintf(`
+	{
 		"msg_body": {"%s": %s},
 		"user_id": "%s"
-	  }`, msgType, msgBody, userID)
+	}`, msgType, msgBody, userID)
 
 	respBytes, err := x.HTTPClient.Request("POST", url, []byte(input), 1)
 	if err != nil {
